5183e4e9ef2d989506e72b64baf5bbbc83ca9a7e
me-high <moraruimihai@gmail.com>
2014-09-08 15:19:42 +0300

    MM: further work on Cucumber

--- src/test/java/AmazonBDD/GenericTest.java
+++ src/test/java/AmazonBDD/GenericTest.java
@@ -7,13 +7,9 @@ import cucumber.api.CucumberOptions;
 import cucumber.api.junit.Cucumber;
 import org.junit.runner.RunWith;
 
-/**
- * Created by Mihai on 07.09.2014.
- */
-
     @RunWith(Cucumber.class)
     //@Cucumber.Options(format = {"pretty", "html:target/cucumber-html-report", "json-pretty:target/cucumber-report.json"})
-    @CucumberOptions(format = "pretty")
+    @CucumberOptions(format = "pretty", features = "src/test/resources/AmazonBDD")
     //this is an empty class to run with. This needs to remain empty
     public class GenericTest
     {
--- src/test/resources/AmazonBDD/Search.feature
+++ src/test/resources/AmazonBDD/Search.feature
@@ -2,8 +2,7 @@ Feature: Testing for search functionality
 
 
 	Scenario Outline: Search Product
-
-		Given I want to use the browser [browser]
+		Given I want to use the browser Firefox
 		When I type ipod in the search input field
 		And I click the Go button
 		Then I am on the search results page
